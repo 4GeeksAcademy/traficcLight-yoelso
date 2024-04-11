ba1d1110e2603ac1df5e888fdf27a7f94b86904e
Adrian <142854436+withyso@users.noreply.github.com>
2024-04-11 02:55:45 +0000

    handleClickColor function added

--- src/js/component/TraficcLight.js
+++ src/js/component/TraficcLight.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react'
 
 export const TraficcLight = () => {
-  const [red, setRed] = useState('selected')
-  const [yellow, setYellow] = useState('')
-  const [green, setGreen] = useState('')
+  const [red, setRed] = useState('red-light')
+  const [yellow, setYellow] = useState('yellow-light')
+  const [green, setGreen] = useState('green-light')
+  const [color, setColor] = useState('red-light')
   const [isSelected, setIsSelected] = useState(false)
+
+  const handleClickColor = (newColor) => {
+    if (color == newColor) {
+      setColor('')
+    }
+    else { setColor(newColor) }
+  }
 
 
   return (
@@ -12,23 +20,35 @@ export const TraficcLight = () => {
       <div className='top-traffic-light bg-dark'></div>
       <div className='body-traffic-light rounded-4 bg-dark'>
         <div className='lights d-flex flex-column align-items-center gap-1 mt-4'>
-          <div className={`red-light rounded-circle bg-danger ${red}`} onClick={() => {
-            setRed('selected')
-            setYellow('')
-            setGreen('')
+          <div className={`red-light rounded-circle bg-danger  ${color == 'red-light' ? 'selected' : ''}`} onClick={() => {
+            handleClickColor('red-light')
+            // setYellow('')
+            // setGreen('')
+            //setIsSelected(!isSelected)
+            // isSelected == true ? setRed('') : setRed('selected')
+            // setIsSelected(false)
+            // isSelected == false ? setIsSelected(true) : setIsSelected(false);
           }}></div>
-          <div className={`yellow-light rounded-circle bg-warning ${yellow}`} onClick={() => {
-            setRed('')
-            setYellow('selected')
-            setGreen('')
+          <div className={`yellow-light rounded-circle bg-warning ${color == 'yellow-light' ? 'selected' : ''}`} onClick={() => {
+            // setRed('')
+            // setYellow('')
+            handleClickColor('yellow-light')
+            // setGreen('')
+            // isSelected == true ? setYellow('') : setYellow('selected')
+            // setIsSelected(false)
+            // isSelected == false ? setIsSelected(true) : setIsSelected(false);
           }}></div>
-          <div className={`green-light rounded-circle bg-success ${green}`} onClick={() => {
-            setRed('')
-            setYellow('')
-            setGreen('selected')
+          <div className={`green-light rounded-circle bg-success  ${color == 'green-light' ? 'selected' : ''}`} onClick={() => {
+            // setRed('')
+            // setYellow('')
+            // setGreen('selected')
+            handleClickColor('green-light')
+            // isSelected == true ? setGreen('') : setGreen('selected')
+            // setIsSelected(false)
+            // isSelected == false ? setIsSelected(true) : setIsSelected(false);
           }}></div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
